5c804c23ba6cd4b39cb107ba40f4582f3e4053bb
kch_cp <SND\kch_cp>
2011-11-29 17:55:04 -0800

    Minor bug fixes to console applications. UI formatting update. Major reorganization of source directory to facilitate future branching and development. Added incomplete set of unit tests.

---
+++ Seqcos/Main/Source/Application/SeqcosApp/Analyzer/SequenceContaminationFinder.cs
@@ -0,0 +1,118 @@
+﻿// *********************************************************************
+// 
+//     Copyright (c) 2011 Microsoft. All rights reserved.
+//     This code is licensed under the Apache License, Version 2.0.
+//     THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//     ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//     IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//     PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+// 
+// *******************************************************************
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Bio;
+using Bio.Algorithms.Alignment;
+using Bio.IO;
+using Bio.SimilarityMatrices;
+using SeqcosApp.Analyzer.NCBI;
+using SeqcosApp.Properties;
+using System.Threading;
+
+namespace SeqcosApp.Analyzer
+{
+    /// <summary>
+    /// Class that searches for sequence contaminants in the input data, such as
+    /// adapters and/or primers.
+    /// </summary>
+    public class SequenceContaminationFinder
+    {
+
+        #region Members
+
+        public readonly IEnumerable<ISequence> TargetSequences; // target == input sequences
+
+        // SW alignment-related members
+        //private string illuminaContaminantsFile = "../Univec/UniVec_Core.fasta";
+        //public IList<ISequence> ReferenceSequences { get; private set; } // adapter sequences
+        //public Dictionary<ISequence, List<ISequence>> ContaminatedSequences { get; private set; }
+
+        // BLAST-related members
+        public BlastLocalHandler Blaster { get; private set; }
+
+        // Dictionary where key is the subject (i.e. database sequence) sequence name, and formatAsString
+        // is a list of query sequence IDs that have a HSP with the subject.
+        public Dictionary<string, List<string>> BlastHspCounter { get; private set; }
+
+        private const double minimumScoreWeight = 0.6; // weight used to calculate the minimum allowed score for best matches
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Compare input sequences with a list of known contaminants.
+        /// </summary>
+        /// <param name="targetParser">The target (aka input) sequences</param>
+        public SequenceContaminationFinder(ISequenceParser targetParser)
+        {
+            if (targetParser == null)
+                throw new ArgumentNullException("targetParser");
+
+            // Load target (aka input sqeuences) from file
+            this.TargetSequences = targetParser.Parse();
+
+            // Initially set this as null until a BLAST Xml file is processed.
+            this.BlastHspCounter = new Dictionary<string,List<string>>();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Execute a local BLAST against a sequence database. 
+        /// </summary>
+        /// <param name="db">Name of the BLAST database (use filename only).</param>
+        /// <param name="targetFasta">Filename of the input FASTA file.</param>
+        public void RunLocalBlast(string db, string targetFasta)
+        {
+            this.Blaster = new BlastLocalHandler();
+
+            #region BLAST parameters
+
+            IBlastParameters args;
+
+            if (db.Equals(Resource.UniVec) || db.Equals(Resource.UniVec_Core))
+            {
+                args = new UniVecParameters();
+            }
+            else
+            {
+                args = new BlastLocalParameters();
+            }
+
+            args.Database = db;
+            args.Threads = Environment.ProcessorCount;
+            string outputFilename = Path.ChangeExtension(targetFasta, Properties.Resource.BLAST_OUTPUT_SUFFIX_ASN);
+
+            #endregion
+
+            this.Blaster.ExecuteLocal(targetFasta, outputFilename, args);
+
+            // Convert to XML
+            string xmlFilename = Path.ChangeExtension(outputFilename, Resource.XmlExtension);
+            BlastTools.FormatASNTo("5", outputFilename, xmlFilename);
+            this.BlastHspCounter = BlastTools.ProcessBlastXml(xmlFilename);
+
+            // Convert to CSV
+            string csvFilename = Path.ChangeExtension(outputFilename, Resource.CsvExtension);
+            BlastTools.FormatASNTo("10", outputFilename, csvFilename);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Seqcos/Main/Tests/SeqcosApp.Tests/BioHelperTest.cs
@@ -0,0 +1,95 @@
+﻿using SeqcosApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using Bio;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for BioHelperTest and is intended
+    ///to contain all BioHelperTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BioHelperTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Verifies that the FastqFormat enum is returned
+        ///</summary>
+        [TestMethod()]
+        public void GetQualityFormatTypeTest()
+        {
+            string formatAsString = "Solexa";
+            FastQFormatType expected = FastQFormatType.Solexa; 
+            FastQFormatType actual;
+            actual = BioHelper.GetQualityFormatType(formatAsString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies that method returns all expected Fastq formats as a string array
+        ///</summary>
+        [TestMethod()]
+        public void QueryValidFastqFormatsTest()
+        {
+            string[] expected = new string[] { "Illumina", "Solexa", "Sanger" };
+            string[] actual;
+            actual = BioHelper.QueryValidFastqFormats();
+            Assert.AreEqual(expected.Length, actual.Length);
+            foreach (string format in actual)
+            {
+                Assert.IsTrue(expected.Any(s => s.Equals(format)));
+            }
+        }
+
+        /// <summary>
+        /// Verify that the generic all files filter is returned
+        ///</summary>
+        [TestMethod()]
+        public void QuerySupportedFileFiltersTest()
+        {
+            string all = "All files (*.*)|*.*";
+            List<string> actual;
+            actual = BioHelper.QuerySupportedFileFilters();
+            Assert.IsTrue(actual.Any(s => s.Equals(all)));
+        }
+
+        /// <summary>
+        ///A test for GetStringSequence
+        ///</summary>
+        [TestMethod()]
+        public void GetStringSequenceTest()
+        {
+            ISequence seqObj = new QualitativeSequence(Alphabets.DNA, FastQFormatType.Sanger,
+                                            "GGCGCACTTACACCCTACATCCATTG", "IIIIG1?II;IIIII1IIII1%.I7I");
+            string expected = "GGCGCACTTACACCCTACATCCATTG";
+            string actual;
+            actual = BioHelper.GetStringSequence(seqObj);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
